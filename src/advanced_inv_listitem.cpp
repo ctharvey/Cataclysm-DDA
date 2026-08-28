54122e5fe23e67071a98785d0ed99c3197d5e101
ctharvey <ctharvey@me.com>
2026-08-27 21:47:05 -0500

    refactor: derive AIM row endpoint from item location

--- src/advanced_inv_listitem.cpp
+++ src/advanced_inv_listitem.cpp
@@ -1,9 +1,53 @@
 #include "advanced_inv_listitem.h"
 
+#include <optional>
+
+#include "advanced_inv_area.h"
 #include "auto_pickup.h"
 #include "cata_assert.h"
 #include "item.h"
 #include "item_tname.h"
+#include "map.h"
+#include "vehicle_selector.h"
+
+namespace
+{
+std::optional<advanced_inv_endpoint> endpoint_for_item( const item_location &loc,
+        aim_location area )
+{
+    switch( area ) {
+        case AIM_INVENTORY:
+            return advanced_inv_endpoint::inventory();
+        case AIM_WORN:
+            return advanced_inv_endpoint::worn();
+        case AIM_CONTAINER:
+            if( loc.has_parent() ) {
+                return advanced_inv_endpoint::item_container( loc.parent_item() );
+            }
+            return std::nullopt;
+        default:
+            break;
+    }
+
+    switch( loc.where_recursive() ) {
+        case item_location::type::map:
+            return advanced_inv_endpoint::ground( loc.pos_bub( get_map() ) );
+        case item_location::type::vehicle: {
+            const vehicle_cursor *cursor = loc.veh_cursor();
+            if( cursor != nullptr ) {
+                return advanced_inv_endpoint::vehicle_cargo( &cursor->veh,
+                        static_cast<int>( cursor->part ) );
+            }
+            return std::nullopt;
+        }
+        case item_location::type::character:
+        case item_location::type::container:
+        case item_location::type::invalid:
+            return std::nullopt;
+    }
+    return std::nullopt;
+}
+} // namespace
 
 advanced_inv_listitem::advanced_inv_listitem( const item_location &an_item, int index, int count,
         aim_location area, bool from_vehicle )
@@ -19,6 +63,7 @@ advanced_inv_listitem::advanced_inv_listitem( const item_location &an_item, int 
     , weight( an_item->weight() * stacks )
     , cat( &an_item->get_category_of_contents() )
     , from_vehicle( from_vehicle )
+    , endpoint( endpoint_for_item( an_item, area ) )
 {
     items.push_back( an_item );
     cata_assert( stacks >= 1 );
@@ -38,7 +83,8 @@ advanced_inv_listitem::advanced_inv_listitem( const std::vector<item_location> &
     volume( list.front()->volume() * stacks ),
     weight( list.front()->weight() * stacks ),
     cat( &list.front()->get_category_of_contents() ),
-    from_vehicle( from_vehicle )
+    from_vehicle( from_vehicle ),
+    endpoint( endpoint_for_item( list.front(), area ) )
 {
     cata_assert( stacks >= 1 );
 }
